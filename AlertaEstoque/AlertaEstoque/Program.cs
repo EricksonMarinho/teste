cfff4b15aaf0d33911602530cf11592ef7cbb0e2
Erickson Marinho <ericksonmarinho33@gmail.com>
2018-04-24 08:33:17 -0300

    Atividade Pronta

---
+++ AlertaEstoque/AlertaEstoque/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AlertaEstoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double qtdMaxima, qtdAtual, resultado;
+            string nomeProduto;
+            Console.Clear();
+
+            Console.WriteLine("Digite o nome do produto");
+            nomeProduto = Console.ReadLine();
+
+            Console.WriteLine("Digite a quantidade atual do produto");
+            qtdAtual = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade máxima do produto");
+            qtdMaxima = double.Parse(Console.ReadLine());
+
+            resultado = (qtdAtual / qtdMaxima)*100;
+
+            Console.WriteLine(resultado.ToString("n2")+"%");
+
+            if(resultado <= 12)
+                Console.WriteLine("Comprar com Urgência");
+            else if(resultado<=20)
+                Console.WriteLine("Realizar Compra");
+            else
+                Console.WriteLine("Ponto de Utilização");
+        }
+    }
+}
---
+++ Caixa/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace Caixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string produto;
+           double preco;
+           bool forma_de_pagamento;
+           double resultado;
+           double precofinal;
+
+           Console.WriteLine("Digite o nome do produto:");
+           produto = Console.ReadLine();
+           
+           Console.WriteLine("Digite o preço do produto:");
+           preco = double.Parse(Console.ReadLine());
+           
+           Console.WriteLine("O pagamento será em dinheiro");
+           forma_de_pagamento = bool.Parse(Console.ReadLine());
+
+           if (forma_de_pagamento==true){
+               resultado = preco * 10/100;
+               precofinal = preco-resultado;
+           }
+           else{
+               resultado = preco * 5/100;
+               precofinal = preco+resultado;
+           }
+
+           StreamWriter arquivo = new StreamWriter("dados.txt");
+           arquivo.WriteLine("O produto escolhido foi: "+produto);
+           arquivo.WriteLine("O preço a vista do produto é: "+preco);
+           arquivo.WriteLine("O valor de desconto ou acréscima é: "+resultado);
+           arquivo.WriteLine("O preço final do produto é: "+precofinal);
+           arquivo.Close();
+
+
+           Console.WriteLine("O produto escolhido foi: "+produto);
+           Console.WriteLine("O preço a vista do produto é: "+preco);
+           Console.WriteLine("O valor de desconto ou acréscima é: "+resultado);
+           Console.WriteLine("O preço final do produto é: "+precofinal);
+        }
+    }
+}
